8ac897b871a07dd4cdc6522c4a47fe9cb2888898
Jack <jackchow1985@gmail.com>
2015-11-09 00:06:51 +0800

    add user risk score page

--- public/javascripts/integrate.js
+++ public/javascripts/integrate.js
@@ -8,18 +8,14 @@ newsApp.controller('NewsListController',
     $scope.loadData = function(chnId, listTarget) {
       $scope[listTarget] = [];
       $scope["loaded" + chnId] = false;
-      if(chnId == 0) { // fx
-
-      
-        $http.get('/getMergedNews?chnId=' + chnId).success(function(data) {
-          for(var i = 0 ; i < data.result.length; i ++) {
-            if(data.result[i].images && data.result[i].images.length > 0)
-              data.result[i].images[0] = data.result[i].images[0].replace("&type=webp_270x190", '')
-          }
-          $scope[listTarget] = data.result;
-          $scope["loaded" + chnId] =true;
-        }); 
-      }
+      $http.get('/getNews?chnId=' + chnId).success(function(data) {
+        for(var i = 0 ; i < data.result.length; i ++) {
+          if(data.result[i].images && data.result[i].images.length > 0)
+            data.result[i].images[0] = data.result[i].images[0].replace("&type=webp_270x190", '')
+        }
+        $scope[listTarget] = data.result;
+        $scope["loaded" + chnId] =true;
+      });
     }
     $scope.loadData(0, "newsList0"); // 外汇
     $scope.loadData(1, "newsList1"); // 期货
--- routes/index.js
+++ routes/index.js
@@ -11,10 +11,26 @@ router.get('/', function(req, res, next) {
 router.get('/getNews', function(req, res, next) {
 	request("http://o.go2yd.com/oapi/hkfdt/news_list_for_finance?channel_id=" + req.query["chnId"],function(error, response, html) {
 		var jsonObj = JSON.parse(html);
-		res.json(jsonObj)
-		
+		res.json(jsonObj)	
 	})
   	//res.render('news', { title: 'News from Yidian' });
 });
+
+router.get('/getMergedNews', function(req, res, next) {
+	request("http://121.43.73.191:8080/news?pages=2&category_id=" + req.query["chnId"],function(error, response, html) {
+		var jsonObj = JSON.parse(html);
+		res.json(jsonObj)	
+	})
+  	//res.render('news', { title: 'News from Yidian' });
+});
+
+function _getData(chnId, dataArr, cb) {
+	console.info(dataArr.length)
+	request("http://o.go2yd.com/oapi/hkfdt/news_list_for_finance?channel_id=" + chnId, function(error, response, html) {
+		var thisArr = JSON.parse(html).result;
+		var dataArr = thisArr.concat(dataArr)
+		cb(dataArr)
+	});
+}
 
 module.exports = router;
